23d5ae9e4271642e6a211a9e1c79af22edf0a06f
kennycummings <kennethccummings@gmail.com>
2024-01-09 19:52:54 -0500

    Updated ProductTag.js

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -16,17 +16,17 @@ ProductTag.init(
     product_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'product',
+        model: Product,
         key: 'id',
       },
     },
     tag_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'tag',
+        model: Tag,
         key: 'id',
       },
-    },
+    },    
   },
   {
     sequelize,
